b687cbfb14cd97b091b2baa5757f484416ae4268
js223mx <js223mx@student.lnu.se>
2015-01-07 01:52:42 +0000

    added window

--- 5-pwd/js/main.js
+++ 5-pwd/js/main.js
@@ -3,5 +3,5 @@
 var clickIt = document.getElementById('menuicon');
 
 clickIt.onclick = function(){
-    
+   window.open("../window.html", "_blank", "toolbar=yes, scrollbars=yes, resizable=yes, top=500, left=500, width=400, height=400");
 };
